de1144648168e93dd304c8d39a041929c6830926
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-08-13 08:04:07 -0600

    all problems done

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,8 +1,45 @@
 import React, { Component } from "react";
 
 class Palindrome extends Component {
+  state = {
+    userInput: "",
+    palindrome: "",
+  };
+
+  handleChange = (val) => {
+    this.setState({ userInput: val });
+  };
+
+  handleClick = (input) => {
+    let palindrome = [];
+    input.split("").map((i) => {
+      palindrome.unshift(i);
+    });
+    palindrome.join();
+    this.setState({ palindrome: palindrome });
+  };
+
   render() {
-    return <p>Palindrome</p>;
+    return (
+      <div className="puzzleBox filterStringPB">
+        <h4>Palindrome</h4>
+        <input
+          className="inputLine"
+          onChange={(e) => {
+            this.handleChange(e.target.value);
+          }}
+        />
+        <button
+          className="confirmationButton"
+          onClick={() => {
+            this.handleClick(this.state.userInput);
+          }}
+        >
+          Flip
+        </button>
+        <span className="resultsBox">{this.state.palindrome}</span>
+      </div>
+    );
   }
 }
 
--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,8 +1,47 @@
 import React, { Component } from "react";
 
 class Sum extends Component {
+  state = { num1: 0, num2: 0, sum: null };
+
+  handleChange = (val) => {
+    this.setState({ num1: val });
+  };
+  handleChange2 = (val) => {
+    this.setState({ num2: val });
+  };
+
+  handleClick = (input1, input2) => {
+    let sum = parseInt(input1) + parseInt(input2);
+    this.setState({ sum: sum });
+  };
+
   render() {
-    return <p>Sum</p>;
+    return (
+      <div className="puzzleBox filterStringPB">
+        <h4>Sum</h4>
+        <input
+          className="inputLine"
+          onChange={(e) => {
+            this.handleChange(e.target.value);
+          }}
+        />
+        <input
+          className="inputLine"
+          onChange={(e) => {
+            this.handleChange2(e.target.value);
+          }}
+        />
+        <button
+          className="confirmationButton"
+          onClick={() => {
+            this.handleClick(this.state.num1, this.state.num2);
+          }}
+        >
+          Sum
+        </button>
+        <span className="resultsBox">{this.state.sum}</span>
+      </div>
+    );
   }
 }
 
